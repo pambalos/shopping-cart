8eb2e093c0fd1cae010302f5d0bc7ddec7a91888
pambalos <bradleyjusticeca@gmail.com>
2018-08-07 16:33:13 -0700

    Work from throughout the day

--- cartapp/routes.py
+++ cartapp/routes.py
@@ -9,8 +9,23 @@ def shop():
     cart = Order()
     return render_template('index.html', title = 'Shop', cart = cart)
 
+
+@app.route('/home')
+def home():
+    return render_template('home.html', title = 'Home')
+
 @app.route('/dev')
 def check():
     form = OrderForm()
     cart = Order()
     return render_template('cartForm.html', title = 'Dev', cart = cart, form = form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = "Login", form = form)
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title = 'Register', form = form)
