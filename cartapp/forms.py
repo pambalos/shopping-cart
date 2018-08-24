bfde6d260d04c11ed23a5f61d63b00bd53e0f73b
pambalos <bradleyjusticeca@gmail.com>
2018-08-23 20:13:06 -0700

    added some styling and slight form editing

--- cartapp/forms.py
+++ cartapp/forms.py
@@ -28,8 +28,7 @@ class ProfileForm(FlaskForm):
 class LoginForm(FlaskForm):
     email = StringField("Email", validators = [DataRequired(), Email()])
 
-    password = PasswordField("Password",
-                            validators = [DataRequired()])
+    password = PasswordField("Password", validators = [DataRequired()])
 
     remember = BooleanField("Remember Me")
     submit = SubmitField("Login")
